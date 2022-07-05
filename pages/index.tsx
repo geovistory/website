a2cde045fe07de6bb2fc5828376efa2459e6c2ad
joschne <jonas.schneider@balcab.ch>
2022-07-05 13:16:40 +0200

    add version number

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import { version } from '../package.json';
 
 const Home: NextPage = () => {
   return (
-    <div style={{width: '100%', height: '95vh', display: 'flex', justifyContent: 'center'}}>
-      <Image src="/geovistory-logo.svg" width={300} height={100}></Image>
+    <div
+      style={{
+        width: '100%',
+        height: '95vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#aaa'
+      }}
+    >
+      <Image src="/geovistory-logo.svg" alt="Geovistory logo" width={300} height={100}></Image>
+      <div> v{version}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
