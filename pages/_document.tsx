ef5e5bcb0846ba8e2473a64b5bb8c183536f5714
joschne <jonas.schneider@balcab.ch>
2022-07-12 13:03:38 +0200

    fix build errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
 
-import { Html, Head, Main, NextScript } from 'next/document'
+const CustomHtml = (props:any) => Html(props);
 
 export default function Document() {
   return (
-    <Html className="ios" mode="ios">
+    <CustomHtml className="ios" mode="ios">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
-  )
+    </CustomHtml>
+  );
 }
