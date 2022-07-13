4d5e928f04220447bbeb3220f1b71624e621705e
joschne <jonas.schneider@balcab.ch>
2022-07-13 08:41:31 +0200

    add cc license component

---
+++ components/elements/CCLicense.component.tsx
@@ -0,0 +1,16 @@
+export const CCLicense = () => {
+  return (
+    <div className="ion-text-end">
+      <small>
+        {'data on this sparql endpoint is licensed as '}
+        <a
+          href="https://creativecommons.org/licenses/by-sa/4.0/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CC BY-SA 4.0
+        </a>
+      </small>
+    </div>
+  );
+};
