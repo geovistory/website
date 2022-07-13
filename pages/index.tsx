4cc16baa9ac1461fb0444aca88560a44a817035c
joschne <jonas.schneider@balcab.ch>
2022-07-13 13:41:44 +0200

    add carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import type { NextPage } from 'next';
 import { DefaultPage } from '../components/layouts/DefaultPage.component';
-
+import { GeovCarousel } from '@geovistory/design-system-react';
 const Home: NextPage = () => {
   return (
     <DefaultPage>
       <h1>Geovistory</h1>
+      <GeovCarousel style={{height:400, maxWidth:600}}
+        images={[
+          '/carousel-1_maquette-lugdunum.jpg',
+          '/carousel-2_tapisserie-bayeux.jpg',
+          '/carousel-3_vitrail.jpg',
+          '/carousel-4_astrolabe-arabe.jpg',
+          '/carousel-5_quetzalcoatl.jpg',
+          '/carousel-6_bronzes-benin.jpg',
+          '/carousel-7_carte-amerique.jpg',
+          '/carousel-8_assemble-vizille.jpg',
+          '/carousel-9_acte-deces.jpg',
+          '/carousel-10_guerre-russo-japonaise.jpg',
+          '/carousel-11_congret-musique.jpg',
+          '/carousel-12_toronto.jpg',
+        ]}
+      ></GeovCarousel>
       <p> some text</p>
       <p> some text</p>
       <p> some text</p>
