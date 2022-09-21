35b1ce407df283722ff0bf6310bb80ab57797f32
joschne <jonas.schneider@balcab.ch>
2022-09-21 11:41:54 +0200

    improve typography

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-const CustomHtml = (props:any) => Html(props);
+const CustomHtml = (props: any) => Html(props);
 
 export default function Document() {
   return (
     <CustomHtml className="ios" mode="ios">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
