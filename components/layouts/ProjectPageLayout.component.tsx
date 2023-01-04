5871f308e541ab0d38b2ecd048ffc3bb2d645bd9
joschne <jonas.schneider@balcab.ch>
2023-01-04 16:03:47 +0100

    add sparklis

--- components/layouts/ProjectPageLayout.component.tsx
+++ components/layouts/ProjectPageLayout.component.tsx
@@ -38,6 +38,9 @@ export const ProjectPageLayout = (props: ProjectPageLayoutProps) => {
                   <ion-item href={`/project/${props.navbar.projectId}/sparql`}>
                     SPARQL
                   </ion-item>
+                  <ion-item href={`https://sparklis-dev.geovistory.org/?title=Geovistory%20Project%20${props.navbar.title}&endpoint=https%3A//sparql.geovistory.org/api_v1_project_${props.navbar.projectId}&entity_lexicon_select=http%3A//www.w3.org/2000/01/rdf-schema%23label&concept_lexicons_select=http%3A//www.w3.org/2000/01/rdf-schema%23label`} target="blank">
+                    Sparklis
+                  </ion-item>
                   <ion-item href={`/project/${props.navbar.projectId}/search`}>
                     Search
                   </ion-item>
