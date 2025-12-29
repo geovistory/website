18397bf418254faca12ac6012418bd27c5796c47
dferhod <djamel.ferhod@cnrs.fr>
2025-12-29 15:03:33 +0100

    Added a tab component for the new "data overview" section

--- lib/projectHomePages/objective/objective.tsx
+++ lib/projectHomePages/objective/objective.tsx
@@ -206,10 +206,12 @@ const OBJECTive_component: NextPage<ProjectPageProps> = (props) => {
                   <p className={styles.justify}>
                     The different visualisations are guided by the main research questions:
                   </p>
-                  <ul>
-                    <li>What kind of objects are circulating on the auction market in the time periods? What is their origin of manufacture? Their typology and material? When were they made?</li>
-                    <li>Who are the actors leading the market?</li>
-                  </ul>
+                  <p>
+                    <ul>
+                      <li>What kind of objects are circulating on the auction market in the time periods? What is their origin of manufacture? Their typology and material? When were they made?</li>
+                      <li>Who are the actors leading the market?</li>
+                    </ul>
+                  </p>
                   <p className={styles.justify}>
                     The recorded data emanates from a selection of 28 Parisian auctions, taking place from 1839 until 1895, referencing their catalogues and total of 12 596 lots. The number of auctions doesn’t allow us to reach conclusive broader analysis but this overview of the recorded data can be used to highlight the quality of the information and the research perspectives once more data could be aggregated.
                   </p>
